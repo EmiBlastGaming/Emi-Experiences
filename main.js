463f199274d990a2bcf5e2050d1be8c57d383713
EmiBlastGaming <emiblastgaming@gmail.com>
2024-05-01 12:09:33 -0600

    Changing access

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function checklogin() {
   var userInput = document.getElementById("login-textbox").value;
 
-  if (userInput === "Micaela Kejner" || userInput === "No pague" || userInput === "No pague1") {
+  if (userInput === "Micaela Kejner" || userInput === "Camilo Kejner" || userInput === "Paulina Sanchez") {
     alert("Lo siento, pero no ha pagado");
     window.location = "nopago.html"; }
-  if (userInput === "Emiliano Kejner" || userInput === "Paulina Sanchez" || userInput === "khzxi") {
+  if (userInput === "Emiliano Kejner" || userInput === "adsjasd" || userInput === "khzxi") {
     alert("Nombre aprobado");
     window.location = "chooseexperience.html";
   } else {
